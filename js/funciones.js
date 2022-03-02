ec6405e07fec8a57d5ab057b1eee572e4f79e1fb
VictorLuna-ISC <75042129+VictorLuna-ISC@users.noreply.github.com>
2022-03-01 20:38:08 -0600

    Cambio de resolucion
    Y Adrian tambien
    
    Co-Authored-By: LuisBA31 <74994999+LuisBA31@users.noreply.github.com>

--- js/funciones.js
+++ js/funciones.js
@@ -3,12 +3,18 @@ function fondo(){
     var f = fondo.getContext("2d");
     var img = new Image();
     img.src = "/recursos/habitat/marcoIndex.png";
+    f.canvas.width  = window.innerWidth;
+    f.canvas.height = window.innerHeight;
     img.onload = function(){
         f.drawImage(img, 0,0,fondo.width,fondo.height);
     }
+    var logo = document.getElementById("fondo");
+    var s = logo.getContext("2d");
+    var ancho  = window.innerWidth/4+75;
+    var alto = window.innerHeight/100-130;
     var logo = new Image();
     logo.src = "/recursos/habitat/logo.png";
     logo.onload = function(){
-        f.drawImage(logo,fondo.width/2,1);
+        s.drawImage(logo,ancho,alto,logo.width*1.5,logo.height*1.5);
     }
 }
