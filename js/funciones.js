cdad8902eec3c8b99f0017555d6789ef6e97fbfa
VictorLuna-ISC <75042129+VictorLuna-ISC@users.noreply.github.com>
2022-03-01 22:13:10 -0600

    Finalizado de la pantalla principal
    También estuvo Adrián.
    
    Co-Authored-By: LuisBA31 <74994999+LuisBA31@users.noreply.github.com>

--- js/funciones.js
+++ js/funciones.js
@@ -2,19 +2,35 @@ function fondo(){
     var fondo = document.getElementById("fondo");
     var f = fondo.getContext("2d");
     var img = new Image();
-    img.src = "/recursos/habitat/marcoIndex.png";
+    img.src = "/recursos/habitat/fondo.png";
     f.canvas.width  = window.innerWidth;
     f.canvas.height = window.innerHeight;
     img.onload = function(){
         f.drawImage(img, 0,0,fondo.width,fondo.height);
     }
-    var logo = document.getElementById("fondo");
-    var s = logo.getContext("2d");
-    var ancho  = window.innerWidth/4+75;
-    var alto = window.innerHeight/100-130;
-    var logo = new Image();
-    logo.src = "/recursos/habitat/logo.png";
-    logo.onload = function(){
-        s.drawImage(logo,ancho,alto,logo.width*1.5,logo.height*1.5);
+}
+/*function validarForm(form) {
+    var correcto=true;
+    var formulario=document.formNombre;
+    var aux=0;
+    var er_tel = /^\d{10}$/
+    for (var i=0; i<formulario.length; i++) {
+        if(formulario.type =='text') {
+            if (formulario[i].value == null || formulario[i].value.length == 0 || /^[A-Z]+$/i.test(formulario.value)){
+                document.getElementById('nombreCompleto').focus();
+                alert ('Por favor complete todos los campos del formulario');
+                correcto=false;
+                break;
+            }
+        }
     }
+    if (correcto ==true) {
+        formulario.submit();
+    }
+}*/
+function guardarNombre() {
+    localStorage.nombre = document.getElementById("nombreCompleto").value;
+    alert("Iniciando juego");
+    document.getElementById("nombreCompleto").value="";
+    window.open('nivel.html','_self');
 }
